1d9aa6453d75e16749ae5f39ce6aaaacdbed85c2
blurk <nsinh6745@gmail.com>
2020-05-17 21:17:29 +0700

    complete lesson 3

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const port = 3000
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+const users = [
+	{id: 1, name: 'Sinh'},
+	{id: 2, name: 'Hang'}	
+];
 
 app.get('/', (req, res) => res.render('index', //path to file
 									 { name: 'Sinh'} //local var
 							))//render from views folder
 
-app.get('/users', (req, res) => res.render('users/index',
-									{ 
-										users: [
-											{id: 1, name: 'Sinh'},
-											{id: 2, name: 'Hang'}	
-										]
-									}	
-							))
+app.get('/users', (req, res) => res.render('users/index',{ users: users }))
 
+app.get('/users/search', (req, res) => {
+	let q = req.query.q.toLowerCase();
+	let matchedUsers = users.filter( (user) => user.name.toLowerCase().indexOf(q) !== -1)
+	res.render('users/index',{ users: matchedUsers, q: q })
+})
+							
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
